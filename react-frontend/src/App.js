683ec053bb8b9f955fe53f68b86433393ff586bd
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2025-06-06 10:39:14 -0700

    map js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Upload from './upload';
+import Map from './map';
 
 const App = () => {
     const [data, setData] = useState('');
