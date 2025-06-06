879fa36b4f75f63df7ff2e8aff8052c0dae43681
audreywtu <audreywtu@gmail.com>
2025-06-06 10:36:54 -0700

    basic ai

--- AI/ai.py
+++ AI/ai.py
@@ -13,7 +13,7 @@ os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY', openaikey)
 
 gpt = OpenAI()
 
-system_message = "Should this item go to a landfill, recycled, or composted: https://assets.fishersci.com/TFS-Assets/CCG/Air-Tite-Products-Co-Inc/product-images/ML320112-B-3.jpg-650.jpg"
+system_message = "Concisely state what the following image is and whether it should go to landfill, recycle, compost, or specific hazardous other: https://energyeducation.ca/wiki/images/thumb/7/74/Batterydura.jpg/1200px-Batterydura.jpg"
 user_prompt = "You are part of a waste sustainability app that analyzes photos."
 
 prompts = [
@@ -21,7 +21,7 @@ prompts = [
     {"role": "user", "content": user_prompt}
   ]
 
-temp = 0.3
+temp = 0.1
 
 def call_gpt(promptsss):
     completion = gpt.chat.completions.create(
